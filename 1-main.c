ea68d289b093da4afcf389156e85f2da29514196
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-02-14 18:16:07 +0100

    [ADD] odd number tasks

--- 1-main.c
+++ 1-main.c
@@ -21,11 +21,3 @@ int main(void)
     binary_tree_print(root);
     return (0);
 }
-alex@/tmp/binary_trees$ gcc -Wall -Wextra -Werror -pedantic binary_tree_print.c 1-main.c 1-binary_tree_insert_left.c 0-binary_tree_node.c -o 1-left
-alex@/tmp/binary_trees$ ./1-left
-  .--(098)--.
-(012)     (402)
-
-       .--(098)-------.
-  .--(054)       .--(402)
-(012)          (128)    
